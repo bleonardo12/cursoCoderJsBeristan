c0e6caf43b851973b884a58bdffa2b9e03fa2ed6
Leonardo Beristain <beristain.lbc@gmail.com>
2021-10-30 10:39:42 -0300

    iniciando proyecto js

---
+++ clase1.js
@@ -0,0 +1,64 @@
+// Desafio 1
+const nombreUsuario = prompt("Ingrese su nombre");
+const ingreseCuit = prompt("Cuit/Cuil");
+const montoFinal = prompt("Ingrese monto final");
+const porcentajeDivisor = prompt("Ingrese numero divisor");
+const total = (montoFinal * porcentajeDivisor) / 100;
+
+alert("Resultado:" + total);
+
+// votacion
+
+// let edadUsuario = prompt("Ingresa tu edad");
+
+// if (edadUsuario >= 16) {
+//     console.log("Puede votar");
+// } else {
+//     console.log("No puede votar");
+// }
+
+// Notas del colegio
+
+// notas mayor o igual a 7 aprobado
+// notas entre 7 y 4 va a recuperatorio
+// let nota = prompt("Ingrese la nota");
+// if (nota >= 7) {
+//     console.log("Aprobado");
+// } else if (nota >= 4) {
+//     console.log("Recuperatorio");
+// } else {
+//     console.log("Desaprobado");
+// }
+
+// programa que pida dos numeros y los sume: informe si la suma es mayor, igual o menor a 100
+
+// let numeroA = prompt("ingrese un numero");
+// let numeroB = prompt("ingrese otro numero");
+
+// let suma = parseInt(numeroA) + parseInt(numeroB);
+
+// console.log(suma);
+
+// if (suma > 100) {
+//     console.log("Es mayor");
+// } else if (suma < 100) {
+//     console.log("Es menor");
+// } else {
+//     console.log("Es igual");
+// }
+
+// CREAR UN PROGRAMA QUE PIDA USUARIO Y CONTRASEÑA
+// SI EL USUARIO ESTA REGISTRADO LO DEJO INGRESAR AL SISTEMA
+// EL UNICO USUARIO REGISTRADO ES usuario=pepe password=1234
+
+// let usuarioRegistrado = "Pepe";
+// let usuarioRegistrado2 = "Jose";
+// let passRegistrado = 1234;
+
+// let usuario = prompt("Ingrese su nombre de usuario");
+// let password = prompt("Ingrese su contraseña");
+
+// if( (usuario == "Pepe" && password == 1234) || (usuario == "Jose" && password == 1234)
+
+// console.log(`Bienvenido al sistema: ${usuario}`);
+// }
