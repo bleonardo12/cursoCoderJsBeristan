22d27d1be920f623fcea4fa48912d19faef1e33a
Leonardo Beristain <beristain.lbc@gmail.com>
2021-11-06 10:34:57 -0300

    entregable 3 JS

---
+++ clase2.js
@@ -0,0 +1,35 @@
+// pedir un numero 8 veces mediante prompt y multiplicarle 2 en cada repeticion, mostrando el resultado por consola, usando for
+
+// Solicitamos un numero al usuario
+// let ingreseNumero = parseInt(prompt("Ingrese un numero"));
+// // En cada repetición multiplicamos el numero por 2,
+// let producto = 1;
+// for (let i = 1; i <= 10; i++) {
+//     let resultado = ingreseNumero * i;
+
+//     alert(ingreseNumero + " X " + i + " = " + resultado);
+// }
+
+// pedir un texto por consola, concatenar un valor en cada iteracción realizando una salida por resultado hasta que se ingresa ESC - ejercicio while
+
+// let texto = prompt("Ingrese una palabra");
+// let resultado = texto;
+// let textoConcatenar = " ";
+
+// while (textoConcatenar != "ESC") {
+//     textoConcatenar = prompt(
+//         "Ingrese una palabra para concatenar a la inicial"
+//     );
+//     resultado = resultado + " " + textoConcatenar;
+//     console.log(resultado);
+// }
+
+// solicitar un numero y detenerse cuando no se trate de un numero; ejercicio do while
+
+// let = 0;
+// do {
+//     // repetimos con do...while mientras se ingrese un number
+//     numero = prompt("Ingrese un numero entero");
+//     console.log(numero);
+//     // Si el ingreso no resulta un numero, se interrumpe el ciclo
+// } while (parseInt(numero));
